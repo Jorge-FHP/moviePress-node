4c744a54aed48dc3a2bf4e39cf5c503471d7f94a
Jorge Hernandez <jnandez565@gmail.com>
2017-09-28 23:59:54 +0000

    movies controller setup done

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 
+// Now instead of explicitly including the controller actions
+// directly inside the server.js file, we can load the
+// controllers/index.js file which will eventually contain
+// all controllers. Doing this will allow us to include all
+// controllers in a more organized manner instead of having
+// them individually included in the server.js
+app.use(require('./controllers'));
+
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
